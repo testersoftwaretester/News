dd8df23d0aa89a30db995c3d876ef9821eedd301
testersoftwaretester <109034320+testersoftwaretester@users.noreply.github.com>
2022-08-17 22:07:44 +0530

    ThisIsAbestDescription

---
+++ ThisANewBug/mains.tf
@@ -0,0 +1,26 @@
+
+resource"azurerm_function_app""Function_application4624810000"  { 
+
+
+
+
+
+}
+ 
+resource"azurerm_function_app_slot""Function_application_slot6132010000"  { 
+
+
+}
+
+ 
+resource"azurerm_function_app_slot""Function_application_slot1919710000"  { 
+
+
+}
+
+ 
+resource"azurerm_function_app_slot""Function_application_slot7240010000"  { 
+
+
+}
+
